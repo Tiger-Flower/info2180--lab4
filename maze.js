03f2c7408d59de04ce2a73eca97c7f42b62795ee
Tiger-Flower <32241839+Tiger-Flower@users.noreply.github.com>
2017-10-20 06:37:51 -0700

    Exercise 3:
     Alerts on successful completion of maze

--- maze.js
+++ maze.js
@@ -1,11 +1,14 @@
 
 window.onload = function(){
+	
 var classname = document.querySelectorAll(".boundary");
+
 
 for (var i = 0; i < classname.length; i++) {
     classname[i].addEventListener('mouseover', youLose);
 }
 
+document.getElementById('end').addEventListener('mouseover', youWin);
 
 };
 
@@ -16,4 +19,12 @@ function youLose(){
 	for (var i = 0; i < classname.length; i++) {
     classname[i].classList.add("youlose");
 	}
+}
+
+
+
+function youWin(){
+	var q =document.getElementsByClassName('youlose');
+	if(q.length == 0)
+		alert( "You win!");
 }
